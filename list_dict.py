f79ad437e0c8e4a838e440dc29487cb3706e4f1f
sixpath <archmage47282@gmail.com>
2026-06-18 20:25:32 +0530

    ADDED MAIN MENU

--- list_dict.py
+++ list_dict.py
@@ -33,7 +33,7 @@ def login(username,writtenpassword):
         print("NO ACCOUNT FOUND CREATE ONE")
         return False
     
-    stored_password = user_db[username][password]
+    stored_password = user_db[username]['password']
     
     if writtenpassword != stored_password:
         print("password didnt match")
@@ -41,5 +41,26 @@ def login(username,writtenpassword):
     print("login successful welcome @",username)
     return True
 
-        
+def main_menu():
+    while True :
+        a = int(input("ENTER A CHOICE\n1.sign in\n2.log in\n3.exit\n"))
+        if a == 3 :
+            break
+        elif a == 1 :
+            # sign in
+            username = input("Enter username ")
+            name =input("enter name ")
+            dob = input("date of birth ")
+            password = input("Enter password ")
+            confirm_password =input("confirm password ")
+            signup(username,name,password,confirm_password,dob)
+
+        elif a  == 2 :
+            username = input("Enter username ")
+            writtenpassword =input("password ")
+            login(username,writtenpassword)
+
+
+        else:
+            print("enter a valid num")
         
